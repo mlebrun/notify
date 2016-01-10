7bc2dbdb02e04efab2450f9414e1054f11926660
Matt LeBrun <lebrun@adobe.com>
2016-01-10 17:16:07 -0500

    Tests: started! covered Primus bindings, also now linking to external repo for Redis subscriber

--- test/Notify.js
+++ test/Notify.js
@@ -1,11 +1,14 @@
 var http = require('http').Server,
     sinon = require('sinon'),
-    expect = require('chai').expect,
-    Notify = require('../'),
+    assert = require('chai').assert,
     Primus = require('primus'),
-    RedisSubscriber = require('../lib/subscribers/Redis'),
+    Emit = require('primus-emit'),
+    Rooms = require('primus-rooms'),
+    Notify = require('../'),
+    RedisSubscriber = require('notify-redis-subscriber'),
+    connected = [],
     opts = { transformer: 'websockets' },
-    srv, subscriber, primus;
+    server, subscriber, primus, client1, client2, client3;
 
 // creates the subscriber
 function getSubscriber(opts) {
@@ -14,38 +17,153 @@ function getSubscriber(opts) {
 
 // creates the client
 function getClient() {
-  var addr = srv.address();
+  var addr = server.address();
   if (!addr) throw new Error('Server is not listening');
   return new primus.Socket('http://localhost:' + addr.port);
 }
 
-// creates the server
+// creates the Primus server
 function getServer(srv, opts) {
-  return new Primus(srv, opts).use('notify', Notify);
+  return new Primus(srv, opts)
+    .use('emit', Emit)
+    .use('rooms', Rooms)
+    .use('notify', Notify);
 }
 
-describe('notify', function () {
+describe('Notify', function () {
   beforeEach(function beforeEach(done) {
-    srv = http();
+    server = http();
     subscriber = getSubscriber(opts);
     opts.subscriber = subscriber;
-    primus = getServer(srv, opts);
-    srv.listen(done);
+    primus = getServer(server, opts);
+
+    sinon.stub(subscriber, 'subscribe', function() {
+      subscriber.emit('subscribed', 'somekey');
+    });
+
+    sinon.stub(subscriber, 'unsubscribe', function() {
+      subscriber.emit('unsubscribed', 'somekey');
+    });
+
+    server.listen(done);
   });
 
   afterEach(function afterEach(done) {
-    primus.end(done);
+    if (client1) {
+      client1.end();
+      client1 = null;
+    }
+
+    if (client2) {
+      client2.end();
+      client2 = null;
+    }
+
+    if (client3) {
+      client3.end();
+      client3 = null;
+    }
+
+    connected = [];
+
+    primus.end(function() {
+      subscriber.subscribe.restore();
+      subscriber.unsubscribe.restore();
+      done();
+    });
   });
 
-  it('should fail if the Subscriber is not an object', function (done) {
-    var alts = { transformer: 'websockets' },
-        altServer = new Primus(srv, alts);
+  describe('Primus Binding', function() {
+    it('should subscribe to a key when a Spark is first to join a room', function(done) {
+      primus.on('connection', function(spark) {
+        connected.push(spark);
 
-    expect(function() {
-      altServer = altServer.use('notify', Notify);
-    }).to.throw(TypeError);
+        if (connected.length < 2) {
+          return;
+        }
 
-    altServer.end(done);
+        setTimeout(function() {
+          assert.isTrue(subscriber.subscribe.calledOnce);
+          assert.isTrue(subscriber.subscribe.calledWith('somekey'));
+          done();
+        }, 100);
 
+        client.emit('subscribe', 'somekey');
+        client2.emit('subscribe', 'somekey');
+      });
+
+      client  = getClient();
+      client2 = getClient();
+    });
+
+    it('should unsubscribe from a key when a Spark is the last to leave a room (leave)', function(done) {
+      primus.on('connection', function(spark) {
+        connected.push(spark);
+
+        if (connected.length < 2) {
+          return;
+        }
+
+        subscriber.on('subscribed', function(room) {
+          connected.forEach(function(s) {
+            s.leave(room);
+          });
+
+          setTimeout(function() {
+            assert.isTrue(subscriber.unsubscribe.calledOnce);
+            assert.isTrue(subscriber.unsubscribe.calledWith('somekey'));
+            done();
+          });
+        });
+
+        client.emit('subscribe', 'somekey');
+      });
+
+      client  = getClient();
+      client2 = getClient();
+    });
+
+    it('should unsubscribe from a key when a Spark is the last to leave a room (leaveAll)', function(done) {
+      primus.on('connection', function(spark) {
+        connected.push(spark);
+
+        if (connected.length < 2) {
+          return;
+        }
+
+        subscriber.on('subscribed', function(room) {
+          connected.forEach(function(s) {
+            s.leaveAll();
+          });
+
+          setTimeout(function() {
+            assert.isTrue(subscriber.unsubscribe.calledOnce);
+            assert.isTrue(subscriber.unsubscribe.calledWith('somekey'));
+            done();
+          });
+        });
+
+        client.emit('subscribe', 'somekey');
+      });
+
+      client  = getClient();
+      client2 = getClient();
+    });
+  });
+
+  describe('Subscriber Binding', function() {
+    it('should track when a key is subscribed to', function(done) { done(); });
+
+    it('should track when a key is unsubscribed from', function(done) { done(); });
+
+    it('should notify all Sparks in a room when a messaged is published', function(done) { done(); });
+  });
+
+  describe('Getter Methods', function() {
+    it('should return a list of all existing subscriptions', function(done) { done(); });
+
+    it('should return a list of all connected clients', function(done) { done(); });
+
+    it('should return a list of all existing rooms', function(done) { done(); });
   });
 });
